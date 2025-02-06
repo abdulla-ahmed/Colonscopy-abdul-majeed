638d477805c6533791c0269ee2a741e0e6706ce7
abdulla-ahmed <83808860+abdulla-ahmed@users.noreply.github.com>
2025-02-06 13:52:16 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ const ASSETS_TO_CACHE = [
   '/Colonscopy-abdul-majeed/',
   '/Colonscopy-abdul-majeed/index.html',
   '/Colonscopy-abdul-majeed/app.js',
-  '/Colonscopy-abdul-majeed/Medical info.json', // Animation file
+  '/Colonscopy-abdul-majeed/Medical Info.json', // Animation file
   '/Colonscopy-abdul-majeed/images/icon-192x192.png',
   '/Colonscopy-abdul-majeed/images/icon-512x512.png'
 ];
