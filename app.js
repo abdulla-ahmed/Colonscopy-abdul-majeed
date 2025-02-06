58dade6ac5fff51613fc9b746ffa03c4f1b9ea4e
abdulla-ahmed <83808860+abdulla-ahmed@users.noreply.github.com>
2025-02-06 13:49:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     renderer: 'svg',
     loop: false,
     autoplay: true,
-    path: 'Medical info.json'
+    path: 'Medical Info.json'
   });
 
   anim.addEventListener('DOMLoaded', () => {
